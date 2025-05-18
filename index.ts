46feaa0d0e8b6e81e99c27f088cc5bafc981fe3b
Innei <tukon479@gmail.com>
2025-05-18 23:19:17 +0800

    update
    Signed-off-by: Innei <tukon479@gmail.com>

--- index.ts
+++ index.ts
@@ -1,6 +1,1 @@
-import { ExtendedValidationPipe } from './pipe'
-
-const use = (a: any) => Object.assign(a, {})
-const pipe = () => use(ExtendedValidationPipe.shared)
-
-export { pipe }
+export { ExtendedValidationPipe } from './pipe'
